96304d983224b51991c2396c8df4744e942eab10
Tomoya Takeda <tomoya.leo9@gmail.com>
2024-07-30 14:29:41 +0900

    dev optimization.java ...

--- reproduce00/main/AdminGame.java
+++ reproduce00/main/AdminGame.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.List;
+
 import javax.naming.spi.DirStateFactory.Result;
 import utils.matrix_util;
 import machines.*;
@@ -13,18 +15,18 @@ public class AdminGame {
         List<Double> pls = new ArrayList<>();
         pls.add(optimization.computePls(z));
 
-        double[][] L = matrix_util.createL(W, W.size);
+        double[][] L = matrix_util.createL(W, W.length);
 
-        List<Double> disaggs = new ArrayList<>();
-        // ここ実装
+        double[] disaggs = new double[W.length];
+        disaggs=computeDisagreement(z, L);
 
         int i = 0;
         boolean flag = true;
         while(flag){
             System.out.println("Iteration:" + i);
 
-            double[][] Wnew = minW(z, lam, A, reducePls, gam, existing);
-            double[] znew = minZ(Wnew, s);
+            double[][] Wnew = optimization.minW(z, lam, A, reducePls, gam, existing);
+            double[] znew = optimization.minZ(Wnew, s);
 
             if(Math.max(norm(z, znew), norm(W, Wnew)) < 0.5 || i > maxIter - 1){
                 flag = false;
@@ -33,10 +35,35 @@ public class AdminGame {
             z = znew;
             W = Wnew;
             i++;
-            pls.add(computePls(z));
-            L = matrix_util.createL(W, W.size);
-            disaggs.add(computeDisagreement(z, L));
+            pls.add(optimization.computePls(z));
+            L = matrix_util.createL(W, W.length);
+            disaggs = computeDisagreement(z, L);
         }
     return new Result(pls, disaggs, z, W);
+    }
+
+
+    private static double norm(double[] a, double[] b) {
+        double sum = 0.0;
+        for (int i = 0; i < a.length; i++) {
+            sum += Math.pow(a[i] - b[i], 2);
+        }
+        return Math.sqrt(sum);
+    }
+
+    private static double[] computeDisagreement(double[] z, double[][] L){
+        double[] disaggs = new double[z.length];
+        double[] temp = new double[z.length];
+        // calculate L * z
+        for(int i = 0 ; i < z.length; i++){
+            for(int j = 0; j < z.length; j++){
+                temp[i] += L[i][j] * z[j];
+            }
+        }
+        // then calculate z^T * temp
+        for(int i = 0; i < n; i++){
+            disaggs[i] = z[i] * temp[i];
+        }
+        return disaggs;
     }
 }
--- reproduce00/main/machines/optimization.java
+++ reproduce00/main/machines/optimization.java
@@ -1,15 +1,35 @@
 package machines;
 
+import utils.matrix_util;
+
 public class optimization {
     public static double[] minZ(double[][] W, double[] s){
-        return new double[s.length];
+        double[][] L = matrix_util.createL(W, W.length);
+        int n = W.length;
+        double[][] LPlusI = matrix_util.add(W, L);
+
+        // z = (LPlusI)^-1 * s : calculateLE solves this linear equation
+        z = calculateLE();
+        return z;
     }
 
     public static double[][] minW(double[] z, double lam, double[][] A, boolean reducePls, double gam, boolean existing){
+        
         return new double[A.length][A[0].length];
     }
 
     public static double computePls(double[] z){
-        return 0.0;
+        double sum = 0.0;
+        for (double value : z){
+            sum += value;
+        }
+        double ZMean = sum / z.length;
+        
+        double sumSquareDifferences = 0.0;
+        for(double value : z){
+            double difference = value - ZMean;
+            sumSquareDifferences += difference * difference;
+        }
+        return sumSquareDifferences / z.length;
     }
 }
